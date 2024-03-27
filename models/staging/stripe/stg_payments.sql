45900631f783b264c8ebcd5aac912a1eceb077a7
des.loubser@mantelgroup.co.nz <des.loubser@mantelgroup.co.nz>
2024-03-27 02:25:48 +0000

    after practice

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,1 +1,8 @@
-SELECT * FROM `dbt-tutorial`.stripe.payment
+SELECT 
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+    amount / 100 as amount,
+    created as created_at 
+    FROM {{ source('stripe','payment')}}
